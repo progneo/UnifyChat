9051e8d8484e2f73184741ea485c0da74095912e
ProgNeo <prograneo@gmail.com>
2022-05-15 00:00:23 +0800

    fixed SwipeRefreshLayout height removed AppModule.kt

--- app/src/main/java/com/progcorp/unitedmessengers/App.kt
+++ app/src/main/java/com/progcorp/unitedmessengers/App.kt
@@ -1,10 +1,11 @@
 package com.progcorp.unitedmessengers
 
 import android.app.Application
+import android.os.Build
 import com.progcorp.unitedmessengers.data.TelegramClient
-import com.progcorp.unitedmessengers.di.AppModule
 import com.progcorp.unitedmessengers.util.VKAccountService
 import com.progcorp.unitedmessengers.interfaces.IAccountService
+import org.drinkless.td.libcore.telegram.TdApi
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import java.util.*
@@ -22,7 +23,18 @@ class App : Application() {
         super.onCreate()
         application = this
         setLocale()
-        tgClient = TelegramClient(AppModule.provideTdlibParameters(applicationContext))
+        tgClient = TelegramClient(TdApi.TdlibParameters().apply {
+            apiId = applicationContext.resources.getInteger(R.integer.telegram_api_id)
+            apiHash = applicationContext.getString(R.string.telegram_api_hash)
+            useMessageDatabase = true
+            useSecretChats = true
+            systemLanguageCode = Locale.getDefault().language
+            databaseDirectory = applicationContext.filesDir.absolutePath
+            deviceModel = Build.MODEL
+            systemVersion = Build.VERSION.RELEASE
+            applicationVersion = "0.1"
+            enableStorageOptimizer = true
+        })
         vkAccountService = VKAccountService(getSharedPreferences("vk_account", MODE_PRIVATE))
         vkRetrofit = Retrofit.Builder()
             .baseUrl("https://api.vk.com/method/")
